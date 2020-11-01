4c683c6a70a0fcc16774947f1f8f8fe40f142328
zanuarts <nuartea27@gmail.com>
2020-11-02 01:05:19 +0700

    Add carousel

--- lib/src/ui/pages/home/page_home_dashboard.dart
+++ lib/src/ui/pages/home/page_home_dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:carousel_pro/carousel_pro.dart';
+import 'package:smkdev/src/constants/constant.dart';
 
 class HomeDashboard extends StatefulWidget {
   @override
@@ -7,15 +9,75 @@ class HomeDashboard extends StatefulWidget {
 }
 
 class _HomeDashboardState extends State<HomeDashboard> {
+  Widget imageCarousel = new Container(
+    height: 280,
+    child: new Carousel(
+      boxFit: BoxFit.cover,
+      images: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey
+          ),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
+                child: Text(
+                  "Sekilas Tentang RS. SMKDEV",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
+                child: Text(
+                  "SMKDEV komunitas developer siswa SMK jurusan Rekayasa Perangkat Lunak (RPL), Teknik Komputer dan Jaringan (TKJ) dan Multimedia (MM) dari seluruh Indonesia.",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          )
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.red
+          )
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey
+          )
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.grey
+          )
+        ),
+      ],
+      autoplay: true,
+      animationCurve: Curves.fastOutSlowIn,
+      animationDuration: Duration(milliseconds: 1000),
+      dotBgColor: Colors.transparent,
+      dotPosition: DotPosition.bottomLeft,
+      dotIncreasedColor: colorPrimary,
+      dotSpacing: 18,
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Text("Selamat Datang")
-          ]
-        ),
+      body: ListView(
+        children: <Widget>[
+          imageCarousel,
+          Text("Selamat Datang")
+        ]
       ),
     );
   }
