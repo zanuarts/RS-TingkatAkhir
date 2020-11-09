3b6c7c2f403c12f3aaba53b77ccc8e0d748de6d8
zanuarts <nuartea27@gmail.com>
2020-11-09 17:24:29 +0700

    fix home carousel, cleaning code

--- lib/src/ui/widgets/home/widget_home_carousel.dart
+++ lib/src/ui/widgets/home/widget_home_carousel.dart
@@ -1,15 +1,10 @@
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:smkdev/src/ui/components/cards/cards_carousel.dart';
 
 import '../../../constants/constant.dart';
 
 class HomeCarousel extends StatelessWidget {
-  static final List<String> imgSlider = [
-    'hospital_01.jpg',
-    'hospital_02.jpg',
-    'hospital_03.jpg',
-    'hospital_04.jpg'
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -18,60 +13,13 @@ class HomeCarousel extends StatelessWidget {
         children: [
           Carousel(
             boxFit: BoxFit.cover,
-            // images: [
-            //   Container(
-            //     decoration: BoxDecoration(
-            //       color: Colors.grey
-            //     ),
-            //     child: Column(
-            //       children: <Widget>[
-            //         Padding(
-            //           padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
-            //           child: Text(
-            //             "Sekilas Tentang RS. SMKDEV",
-            //             style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 30,
-            //               fontWeight: FontWeight.bold
-            //             ),
-            //           ),
-            //         ),
-            //         Padding(
-            //           padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-            //           child: Text(
-            //             "SMKDEV komunitas developer siswa SMK jurusan Rekayasa Perangkat Lunak (RPL), Teknik Komputer dan Jaringan (TKJ) dan Multimedia (MM) dari seluruh Indonesia.",
-            //             style: TextStyle(
-            //               color: Colors.white,
-            //               fontSize: 14,
-            //             ),
-            //           ),
-            //         ),
-            //       ],
-            //     )
-            //   ),
-            //   Container(
-            //     decoration: BoxDecoration(
-            //       color: colorPrimary
-            //     )
-            //   ),
-            //   Container(
-            //     decoration: BoxDecoration(
-            //       color: Colors.grey
-            //     )
-            //   ),
-            //   Container(
-            //     decoration: BoxDecoration(
-            //       color: colorPrimary
-            //     )
-            //   ),
-            // ],
             images:[
-              ExactAssetImage('assets/images/hospital_01.jpg'),
-              ExactAssetImage('assets/images/hospital_02.jpg'),
-              ExactAssetImage('assets/images/hospital_03.jpg'),
-              ExactAssetImage('assets/images/hospital_04.jpg')
+              CarouselCards(),
+              CarouselCards(),
+              CarouselCards(),
+              CarouselCards(),
             ],
-            autoplay: true,
+            autoplay: false,
             animationCurve: Curves.fastOutSlowIn,
             animationDuration: Duration(milliseconds: 1000),
             dotBgColor: Colors.transparent,
@@ -79,47 +27,7 @@ class HomeCarousel extends StatelessWidget {
             dotIncreasedColor: colorPrimary,
             dotSpacing: 18,
           ),
-          Positioned(
-            child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20, 30, 20, 10),
-                      child: Text(
-                        
-                        "Sekilas Tentang RS Tingkat Akhir",
-                        
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 26,
-                          fontWeight: FontWeight.bold,
-                          shadows: [
-                            Shadow(
-                                blurRadius: 20.0,
-                                color: Colors.black,
-                                offset: Offset(5.0, 5.0),
-                                ),
-                            ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(20, 10, 20, 20),
-                      child: Text(
-                        "RS Tingkat Akhir merupakan komunitas developer siswa SMK jurusan Rekayasa Perangkat Lunak (RPL), Teknik Komputer dan Jaringan (TKJ) dan Multimedia (MM) dari seluruh Indonesia.",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 14,
-                          shadows: [
-                            Shadow(
-                                blurRadius: 20.0,
-                                color: Colors.black,
-                                offset: Offset(5.0, 5.0),
-                                ),
-                            ],
-                        ),
-                      ),
-                    ),
-                  ]),)
+          
         ],
       )
     );
