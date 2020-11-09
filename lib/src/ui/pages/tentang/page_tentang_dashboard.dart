42e7c7ab9169cb868511bfc389b80b60685bdc03
asbp <guungofficial@gmail.com>
2020-11-09 21:58:53 +0700

    add stub about page

--- lib/src/ui/pages/tentang/page_tentang_dashboard.dart
+++ lib/src/ui/pages/tentang/page_tentang_dashboard.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../constants/constant.dart';
+import '../../components/cards/cards_doctor.dart';
 
 class TentangDashboard extends StatefulWidget {
   @override
@@ -8,8 +12,47 @@ class TentangDashboard extends StatefulWidget {
 class _TentangDashboardState extends State<TentangDashboard> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+        backgroundColor: colorPrimary,
+        appBar: AppBar(
+          backgroundColor: colorPrimary,
+          elevation: 0.0,
+        ),
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Container(
+          padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(bottom: 20),
+                child: Text(
+                  "Tentang Kami",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Container(
+                height: 200,
+                child: ListView(
+                  // This next line does the trick.
+                  scrollDirection: Axis.horizontal,
+                  children: <Widget>[
+                    DoctorCards(),
+                    DoctorCards(),
+                    DoctorCards(),
+                    DoctorCards(),
+                    DoctorCards(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ))));
   }
 }
--- lib/src/ui/widgets/home/widget_home_about.dart
+++ lib/src/ui/widgets/home/widget_home_about.dart
@@ -3,67 +3,83 @@ import 'package:smkdev/src/ui/components/cards/cards_doctor.dart';
 import 'package:smkdev/src/ui/components/cards/cards_about.dart';
 
 import '../../../constants/constant.dart';
+import '../../pages/tentang/page_tentang_dashboard.dart';
 
 class HomeAbout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-            height: 560,
-            decoration: BoxDecoration(
-              color: colorPrimary,
-            ),
-            child: Column(
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(left: 20, top: 20),
-                      child: Text(
-                        "Tentang Kami",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(right: 20, top: 20),
-                      child: Text(
-                        "Selengkapnya",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 14,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
-                  child: AboutCards(),
-                ),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                  child: Container(
-                    height: 200,
-                    child: ListView(
-                      // This next line does the trick.
-                      scrollDirection: Axis.horizontal,
-                      children: <Widget>[
-                        DoctorCards(),
-                        DoctorCards(),
-                        DoctorCards(),
-                        DoctorCards(),
-                        DoctorCards(),
-                      ],
-                    ),
+      height: 560,
+      decoration: BoxDecoration(
+        color: colorPrimary,
+      ),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 20, top: 20),
+                child: Text(
+                  "Tentang Kami",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
                   ),
-                )
-              ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(right: 20, top: 20),
+                child: Material(
+                    color: Colors.transparent,
+                    child: InkWell(
+                      child: Container(
+                        padding: EdgeInsets.all(5),
+                        child: Text(
+                          "Selengkapnya",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        print("More clicked!");
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => TentangDashboard()),
+                        );
+                      },
+                    )),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 20, 20, 10),
+            child: AboutCards(),
+          ),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+            child: Container(
+              height: 200,
+              child: ListView(
+                // This next line does the trick.
+                scrollDirection: Axis.horizontal,
+                children: <Widget>[
+                  DoctorCards(),
+                  DoctorCards(),
+                  DoctorCards(),
+                  DoctorCards(),
+                  DoctorCards(),
+                ],
+              ),
             ),
-          );
+          )
+        ],
+      ),
+    );
   }
 }
