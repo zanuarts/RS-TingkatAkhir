d77ad8eed2be5a7c54021cb2fee03d67a4c25171
zanuarts <nuartea27@gmail.com>
2020-11-14 07:28:16 +0700

    add doctor_home json and cant implement

--- lib/src/ui/widgets/home/widget_home_about.dart
+++ lib/src/ui/widgets/home/widget_home_about.dart
@@ -1,11 +1,28 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:smkdev/src/constants/constant.dart';
+import 'package:smkdev/src/models/home_doctor.dart';
 import 'package:smkdev/src/ui/components/cards/cards_doctor.dart';
 import 'package:smkdev/src/ui/components/cards/cards_about.dart';
 import 'package:smkdev/src/ui/pages/about/page_about_app.dart';
 
 
-class HomeAbout extends StatelessWidget {
+
+class HomeAbout extends StatefulWidget {
+  @override
+  _HomeAboutState createState() => _HomeAboutState();
+}
+
+class _HomeAboutState extends State<HomeAbout> {
+  List<HomeDoctor> parseJson(String response) {
+    if (response == null) {
+      return [];
+    }
+    final parsed = json.decode(response.toString());
+
+    return parsed != null ? parsed.map<HomeDoctor>((json) => new HomeDoctor.fromJson(json)).toList() : null;
+  }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -65,17 +82,32 @@ class HomeAbout extends StatelessWidget {
             padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
             child: Container(
               height: 200,
-              child: ListView(
-                // This next line does the trick.
-                scrollDirection: Axis.horizontal,
-                children: <Widget>[
-                  DoctorCards(),
-                  DoctorCards(),
-                  DoctorCards(),
-                  DoctorCards(),
-                  DoctorCards(),
-                ],
-              ),
+              child: FutureBuilder(
+                future: DefaultAssetBundle.of(context)
+                    .loadString('assets/json/home_doctor.json'),
+                builder: (context, snapshot) {
+                  // Decode the JSON
+                  List<HomeDoctor> homeDoctor = parseJson(snapshot.data.toString());
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (BuildContext context, int id){
+                      return DoctorCards();
+                    },
+                    itemCount: homeDoctor == null ? 0 : homeDoctor.length,
+                  );
+                }
+              )
+              // child: ListView(
+              //   // This next line does the trick.
+              //   scrollDirection: Axis.horizontal,
+              //   children: <Widget>[
+              //     DoctorCards(),
+              //     DoctorCards(),
+              //     DoctorCards(),
+              //     DoctorCards(),
+              //     DoctorCards(),
+              //   ],
+              // ),
             ),
           )
         ],
