637d5fb567fe3df2378792959e7351dd09c9fa17
asbp <guungofficial@gmail.com>
2020-11-15 17:26:26 +0700

    add career cards

---
+++ lib/src/ui/components/cards/cards_career.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:smkdev/src/models/career.dart';
+import 'package:smkdev/src/ui/pages/career/page_career_detail.dart';
+
+class CareerButtonsWidget extends StatelessWidget {
+  final List<Career> list;
+
+  CareerButtonsWidget({@required this.list}) {
+    assert(list != null);
+  }
+
+  @override
+  Widget build(BuildContext context) => Container(
+        child: ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: list.length,
+            itemBuilder: (BuildContext context, int index) {
+              return CareerButtonItem(
+                caption: list[index].nama,
+                image: AssetImage(list[index].image),
+                onClick: () {},
+                event: list[index],
+              );
+            }),
+      );
+}
+
+class CareerButtonItem extends StatelessWidget {
+  final String caption;
+  final ImageProvider<dynamic> image;
+  final Function onClick;
+  final Career event;
+
+  const CareerButtonItem(
+      {@required this.caption,
+      @required this.image,
+      this.onClick,
+      @required this.event});
+
+  Function _processOnTap(Function onClick) {
+    return onClick ?? () {};
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15.0),
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Material(
+          color: Colors.black,
+          child: InkWell(
+            child: Container(
+              height: 200,
+              alignment: Alignment.bottomLeft,
+              child: Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(color: Colors.white),
+                  padding: const EdgeInsets.only(left: 15, top: 15, bottom: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        caption,
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 15),
+                      ),
+                      Text(
+                        event.shortDesc,
+                        style: TextStyle(),
+                      )
+                    ],
+                  )),
+              decoration: new BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    colorFilter: ColorFilter.mode(
+                        Colors.black.withOpacity(0.7), BlendMode.dstATop),
+                    image: image),
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => CareerDetailPage(
+                          event: event,
+                        )),
+              );
+            },
+          ),
+        ));
+  }
+}
--- lib/src/ui/pages/partner/page_partner_dashboard.dart
+++ lib/src/ui/pages/partner/page_partner_dashboard.dart
@@ -4,7 +4,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:smkdev/src/constants/constant.dart';
+import 'package:smkdev/src/models/career.dart';
 import 'package:smkdev/src/models/partner.dart';
+import 'package:smkdev/src/ui/components/cards/cards_career.dart';
 import 'package:smkdev/src/ui/components/cards/cards_partner.dart';
 import 'package:smkdev/src/ui/widgets/widget_header.dart';
 
@@ -14,6 +16,9 @@ class PartnerDashboard extends StatefulWidget {
 }
 
 class _PartnerDashboardState extends State<PartnerDashboard> {
+  List<Partner> partnerList = List<Partner>();
+  List<Career> careerList = List();
+
   List<Partner> parseJson(String response) {
     if (response == null) {
       return [];
@@ -21,20 +26,28 @@ class _PartnerDashboardState extends State<PartnerDashboard> {
     final parsed = json.decode(response.toString());
 
     return parsed != null
-        ? parsed
-            .map<Partner>((json) => new Partner.fromJson(json))
-            .toList()
+        ? parsed.map<Partner>((json) => new Partner.fromJson(json)).toList()
         : null;
   }
-
-  List<Partner> partnerList = List<Partner>();
 
   void getDummyPartner() async {
     var jsonData = await rootBundle.loadString("assets/json/partner.json");
     var decodedJson = json.decode(jsonData);
+
     setState(() {
-      for (int i = 0; i < decodedJson.length; i++) {
-        partnerList.add(Partner.fromJson(decodedJson[i]));
+      for (var line in decodedJson) {
+        partnerList.add(Partner.fromJson(line));
+      }
+    });
+  }
+
+  void getCareers() async {
+    var jsonData = await rootBundle.loadString("assets/json/career.json");
+    var decodedJson = json.decode(jsonData);
+
+    setState(() {
+      for (var line in decodedJson) {
+        careerList.add(Career.fromJson(line));
       }
     });
   }
@@ -43,6 +56,7 @@ class _PartnerDashboardState extends State<PartnerDashboard> {
   void initState() {
     super.initState();
     this.getDummyPartner();
+    this.getCareers();
   }
 
   @override
@@ -52,10 +66,7 @@ class _PartnerDashboardState extends State<PartnerDashboard> {
       appBar: AppBar(
         title: Text(
           "Partner & Career",
-          style: TextStyle(
-            color: Colors.black,
-            fontWeight: FontWeight.bold
-          ),
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
         ),
         backgroundColor: Colors.transparent,
         elevation: 0.0,
@@ -72,72 +83,65 @@ class _PartnerDashboardState extends State<PartnerDashboard> {
         padding: EdgeInsets.all(20),
         children: [
           Container(
-            alignment: Alignment.center, // Make the hint in the middle vertically
+            alignment:
+                Alignment.center, // Make the hint in the middle vertically
             height: 45,
             padding: EdgeInsets.symmetric(horizontal: 20),
             decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(15),
-              boxShadow: [
-                BoxShadow(
-                  blurRadius: 15.0,
-                  color: colorPrimary.withOpacity(0.1),
-                  offset: Offset(2.0, 2.0),
-                )
-              ]
-            ),
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15.0,
+                    color: colorPrimary.withOpacity(0.1),
+                    offset: Offset(2.0, 2.0),
+                  )
+                ]),
             child: Row(
               children: <Widget>[
                 Expanded(
                   child: TextField(
                     onChanged: (value) {},
                     decoration: InputDecoration(
-                      hintText: "Search",
-                      hintStyle:
-                      TextStyle(
-                        color: colorPrimary.withOpacity(0.5)
-                      ),
-                      enabledBorder: InputBorder.none,
-                      focusedBorder: InputBorder.none
-                    ),
+                        hintText: "Search",
+                        hintStyle:
+                            TextStyle(color: colorPrimary.withOpacity(0.5)),
+                        enabledBorder: InputBorder.none,
+                        focusedBorder: InputBorder.none),
                   ),
                 ),
                 IconButton(
-                  icon: Icon(Icons.search, color: colorPrimary),
-                  onPressed: () {}
-                )
+                    icon: Icon(Icons.search, color: colorPrimary),
+                    onPressed: () {})
               ],
             ),
           ),
-          SizedBox(height:10),
+          SizedBox(height: 10),
           Text(
             "Partner",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 120,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int id) {
+                return PartnerCards(
+                  partner: partnerList[id],
+                );
+              },
+              itemCount: partnerList == null ? 0 : partnerList.length,
             ),
           ),
-          SizedBox(height:10),
-          Container(
-          height: 120,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (BuildContext context, int id) {
-              return PartnerCards(
-                partner: partnerList[id],
-              );
-            },
-            itemCount: partnerList == null ? 0 : partnerList.length,
-          ),
-          ),    
-          SizedBox(height:10),
+          SizedBox(height: 10),
           Text(
             "Career",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20
-            ),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
           ),
+          Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: CareerButtonsWidget(list: careerList))
         ],
       ),
     );
